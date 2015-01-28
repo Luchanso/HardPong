20e0497e33f75a6ef98be3f4275173bae2480423
Oleg <oleg@loutchansky.com>
2015-01-28 13:30:22 +0300

    Refactoring

--- Source/Ball.hx
+++ Source/Ball.hx
@@ -1,17 +1,20 @@
-package;
+package ;
 
 
 import openfl.display.Sprite;
 
 class Ball extends Sprite
 {
-
+	
 	public function new() 
 	{
+		
 		super();
+		
 		this.graphics.beginFill(0xffffff);
 		this.graphics.drawCircle(0, 0, 10);
 		this.graphics.endFill();
+		
 	}
 	
 }
--- Source/Main.hx
+++ Source/Main.hx
@@ -1,5 +1,6 @@
 package ;
 
+
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.Lib;
@@ -9,18 +10,24 @@ import openfl.text.TextField;
 import openfl.text.TextFormat;
 import openfl.text.TextFormatAlign;
 
+
 enum GameState {
+	
 	Paused;
 	Playing;
+	
 }
 
 enum Player {
+	
 	Human;
 	AI;
+	
 }
 
 class Main extends Sprite 
 {
+	
 	var inited:Bool;
 	
 	private var platform1:Platform;
@@ -41,11 +48,14 @@ class Main extends Sprite
 	
 	function resize(e) 
 	{
-		if (!inited) init();		
+		
+		if (!inited) init();
+		
 	}
 	
 	function init() 
 	{
+		
 		if (inited) return;
 		inited = true;
 		
@@ -97,14 +107,20 @@ class Main extends Sprite
 		stage.addEventListener(KeyboardEvent.KEY_DOWN, keyDown);
 		stage.addEventListener(KeyboardEvent.KEY_UP, keyUp);
 		this.addEventListener(Event.ENTER_FRAME, everyFrame);
+		
 	}
 	
-	private function setGameState(state:GameState):Void {
+	private function setGameState(state:GameState):Void 
+	{
+		
 		currentGameState = state;
 		updateScore();
-		if (state == Paused) {
+		if (state == Paused) 
+		{
 			messageField.alpha = 1;
-		}else {
+		}
+		else 
+		{
 			messageField.alpha = 0;
 			platform1.y = 200;
 			platform2.y = 200;
@@ -115,40 +131,62 @@ class Main extends Sprite
 			ballMovement.x = direction * Math.cos(randomAngle) * ballSpeed;
 			ballMovement.y = Math.sin(randomAngle) * ballSpeed;
 		}
+		
 	}
 	
-	private function keyDown(event:KeyboardEvent):Void {
-		if (currentGameState == Paused && event.keyCode == 32) { // Space
+	private function keyDown(event:KeyboardEvent):Void 
+	{
+		
+		if (currentGameState == Paused && event.keyCode == 32) 
+		{
 			setGameState(Playing);
-		}else if (event.keyCode == 38) { // Up
+		} 
+		else if (event.keyCode == 38) 
+		{
 			arrowKeyUp = true;
-		}else if (event.keyCode == 40) { // Down
+		} 
+		else if (event.keyCode == 40) 
+		{
 			arrowKeyDown = true;
 		}
+		
 	}
 	
-	private function keyUp(event:KeyboardEvent):Void {
-		if (event.keyCode == 38) { // Up
+	private function keyUp(event:KeyboardEvent):Void 
+	{
+		
+		if (event.keyCode == 38)
+		{
 			arrowKeyUp = false;
-		}else if (event.keyCode == 40) { // Down
-			arrowKeyDown = false;
 		}
+		else if (event.keyCode == 40) 
+		{
+			arrowKeyDown = false;
+		}	
+		
 	}
 	
-	private function everyFrame(event:Event):Void {
-		if (currentGameState == Playing) {
+	private function everyFrame(event:Event):Void 
+	{
+		
+		if (currentGameState == Playing) 
+		{
 			
-			if (arrowKeyUp) {
+			if (arrowKeyUp) 
+			{
 				platform1.y -= platformSpeed;
 			}
-			if (arrowKeyDown) {
+			if (arrowKeyDown) 
+			{
 				platform1.y += platformSpeed;
 			}
 			
-			if (ball.x > 100 && ball.y > platform2.y + 70) {
+			if (ball.x > 100 && ball.y > platform2.y + 70) 
+			{
 				platform2.y += platformSpeed;
 			}
-			if (ball.x > 100 && ball.y < platform2.y + 30) {
+			if (ball.x > 100 && ball.y < platform2.y + 30) 
+			{
 				platform2.y -= platformSpeed;
 			}
 			
@@ -161,11 +199,13 @@ class Main extends Sprite
 			ball.x += ballMovement.x;
 			ball.y += ballMovement.y;
 			
-			if (ballMovement.x < 0 && ball.x < 30 && ball.y >= platform1.y && ball.y <= platform1.y + 100) {
+			if (ballMovement.x < 0 && ball.x < 30 && ball.y >= platform1.y && ball.y <= platform1.y + 100) 
+			{
 				bounceBall();
 				ball.x = 30;
 			}
-			if (ballMovement.x > 0 && ball.x > 470 && ball.y >= platform2.y && ball.y <= platform2.y + 100) {
+			if (ballMovement.x > 0 && ball.x > 470 && ball.y >= platform2.y && ball.y <= platform2.y + 100) 
+			{
 				bounceBall();
 				ball.x = 470;
 			}
@@ -174,53 +214,65 @@ class Main extends Sprite
 			if (ball.x < 5) winGame(AI);
 			if (ball.x > 495) winGame(Human);
 		}
+		
 	}
 	
-	private function bounceBall():Void {
-		var direction:Int = (ballMovement.x > 0)?( -1):(1);
+	private function bounceBall():Void 
+	{
+		
+		var direction:Int = (ballMovement.x > 0) ? ( -1) : (1);
 		var randomAngle:Float = (Math.random() * Math.PI / 2) - 45;
 		ballMovement.x = direction * Math.cos(randomAngle) * ballSpeed;
 		ballMovement.y = Math.sin(randomAngle) * ballSpeed;
+		
 	}
 	
-	private function winGame(player:Player):Void {
-		if (player == Human) {
+	private function winGame(player:Player):Void 
+	{
+		
+		if (player == Human) 
+		{
 			scorePlayer++;
-		} else {
+		}
+		else
+		{
 			scoreAI++;
 		}
 		setGameState(Paused);
+		
 	}
 	
-	private function updateScore():Void {
+	private function updateScore():Void 
+	{
+		
 		scoreField.text = scorePlayer + ":" + scoreAI;
+		
 	}
-
-	/* SETUP */
 
 	public function new() 
 	{
+		
 		super();	
 		addEventListener(Event.ADDED_TO_STAGE, added);
+		
 	}
 
 	function added(e) 
 	{
+		
 		removeEventListener(Event.ADDED_TO_STAGE, added);
 		stage.addEventListener(Event.RESIZE, resize);
-		#if ios
-		haxe.Timer.delay(init, 100); // iOS 6
-		#else
 		init();
-		#end
+		
 	}
 	
 	public static function main() 
-	{
-		// static entry point
+	{		
+
 		Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
 		Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
 		Lib.current.addChild(new Main());
-		//
+
 	}
+	
 }
